9a5b3afd09e1e450d8e954f13de0c2277b02c0ae
oscar <oscar.272731@gmail.com>
2020-06-01 17:50:08 -0400

    Evaluacion 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Practica';
+  arr : any = [{"Nombre": "Pepe", "Edad" : 18}, 
+              {"Nombre": "Juan", "Edad" : 38}, 
+              {"Nombre": "Maria", "Edad":50}]; //Array de objetos
+  contador : number = 0;
+  //key-value pair
+  ngOnInit(){
+    this.arr[0].Nombre;
+    this.arr[0].Edad;
+    console.log("Valores originales", this.arr[0].Nombre, this.arr[0].Edad);
+    
+
+    this.arr[0].Nombre = "Pepito";
+    this.arr[0].Edad = 25;
+
+    console.log("Cambiado",this.arr[0].Nombre, this.arr[0].Edad);
+  }
+
+  insertarPersona(){
+    this.contador += 1;
+    this.contador > 1 ? null :  this.arr.push({"Nombre" : "Pedro","Edad" : 45});
+  }
+
 }
